df4b4a2f6d718173bee5c630723cf9e90fed1462
Daniel Pfisterer <d.pfisterer@powercloud.de>
2020-12-26 13:33:56 +0100

    Remove Plugin

--- plugins/meta-data-fetcher/README.md
+++
@@ -1,3 +0,0 @@
-# Strapi plugin meta-data-fetcher
-
-A quick description of meta-data-fetcher.
--- plugins/meta-data-fetcher/test.js
+++
@@ -1,7 +0,0 @@
-const {fetch} = require('./services/meta-data-fetcher');
-
-
-const data = fetch('https://www.bunte.de/starprofile/helene-fischer.html')
-data.then(d=> {
-  console.log(d);
-})
