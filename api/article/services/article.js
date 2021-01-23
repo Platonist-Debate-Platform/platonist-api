58f75202e00d470baf0eae4e1b29a6930b97ca93
Daniel Pfisterer <d.pfisterer@powercloud.de>
2021-01-23 15:15:23 +0100

    Create meta data fetcher

--- api/article/services/article.js
+++ api/article/services/article.js
@@ -5,4 +5,19 @@
  * to customize this service
  */
 
-module.exports = {};
+const {getMetadata} = require('page-metadata-parser');
+const domino = require('domino');
+const axios = require('axios');
+
+
+module.exports = {
+  /**
+   * meta-data-fetcher.js service
+   *
+   * @description: A set of functions similar to controller's actions to avoid code duplication.
+   */
+  fetchMetaData: async (url) => {
+    const { data } = await axios.get(url);
+    return getMetadata(domino.createWindow(data).document, url);
+  },
+};
