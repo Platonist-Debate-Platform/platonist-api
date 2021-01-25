0f9f0012bf9e0b50eec7c934ab564d508fa34e82
Daniel Pfisterer <info@daniel-pfisterer.de>
2021-01-25 15:32:19 +0100

    Create debate socket events

--- api/debate/models/debate.js
+++ api/debate/models/debate.js
@@ -5,4 +5,22 @@
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    afterCreate: async (result, /* data */) => {
+      if (strapi.emitSocket && strapi.emitSocket.debate && strapi.emitSocket.debate.create) {
+        strapi.emitSocket.debate.create(result);
+      }
+    },
+    afterUpdate: async (result, /* params, data */) => {
+      if (strapi.emitSocket && strapi.emitSocket.debate.update) {
+        strapi.emitSocket.debate.update(result);
+      }
+    },
+    afterDelete: async(result, /* params*/ ) => {
+      if (strapi.emitSocket && strapi.emitSocket.debate.delete) {
+        strapi.emitSocket.debate.delete(result);
+      }
+    }
+  }
+};
