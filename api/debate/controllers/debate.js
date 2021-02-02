c106342e4afcfe7a026ff0c2009b266e059d86bd
Daniel Pfisterer <info@daniel-pfisterer.de>
2021-02-02 09:31:07 +0100

    create an update controller

--- api/debate/controllers/debate.js
+++ api/debate/controllers/debate.js
@@ -1,22 +1,26 @@
-"use strict";
+'use strict';
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-const { sanitizeEntity, parseMultipartData } = require("strapi-utils");
+const { sanitizeEntity, parseMultipartData } = require('strapi-utils');
 
-const { getModel, getService } = require("../../../lib/utils");
+const { getModel, getService } = require('../../../lib/utils');
 
-const settings = require("../models/debate.settings.json");
-const articleSetting = require("../../article/models/article.settings.json");
+const settings = require('../models/debate.settings.json');
+const articleSetting = require('../../article/models/article.settings.json');
 
 const modelName = settings.info.name.toLowerCase();
 const articleModelName = articleSetting.info.name.toLowerCase();
 
+const formatError = error => [
+  { messages: [{ id: error.id, message: error.message, field: error.field }] },
+];
+
 module.exports = {
-  async create(ctx) {
+  create: async (ctx) => {
     const service = getService(strapi, modelName);
     const articleService = getService(strapi, articleModelName);
     const model = getModel(strapi, modelName);
@@ -28,7 +32,7 @@ module.exports = {
       try {
         articleA = await articleService.create(ctx.request.body.articleA);
       } catch (error) {
-        ctx.badRequest(error);
+        ctx.badRequest(formatError(error));
       }
 
       ctx.request.body.articleA = sanitizeEntity(articleA, {
@@ -49,17 +53,27 @@ module.exports = {
       });
     }
 
-    if (ctx.is("multipart")) {
-      const { data, files } = parseMultipartData(ctx);
-      entity = await service.create(data, { files });
-    } else {
-      entity = await service.create(ctx.request.body);
+    const count = await service.count({ title: ctx.request.body.title });
+
+    if (count > 0) {
+      return ctx.badRequest(`Title "${ctx.request.body.title}"  already taken, please choose another!`);
+    }
+
+    try {
+      if (ctx.is('multipart')) {
+        const { data, files } = parseMultipartData(ctx);  
+        entity = await service.create(data, { files });
+      } else {
+        entity = await service.create(ctx.request.body);
+      }
+    } catch (error) {
+      return ctx.badRequest(formatError(error));
     }
 
     return sanitizeEntity(entity, { model });
   },
 
-  async findByTitle(ctx) {
+  findByTitle: async (ctx) => {
     const service = getService(strapi, modelName);
     const model = getModel(strapi, modelName);
     const { title } = ctx.params;
@@ -67,12 +81,91 @@ module.exports = {
     return sanitizeEntity(entity, { model });
   },
 
-  async findComments(ctx) {
+  findComments: async (ctx) => {
     const { id } = ctx.params;
     const entity = await strapi.services.comment.findOne({
       debate: id,
     });
 
     return sanitizeEntity(entity, { model: strapi.models.comment });
+  },
+
+  update: async (ctx) => {
+    const service = getService(strapi, modelName);
+    const articleService = getService(strapi, articleModelName);
+    const model = getModel(strapi, modelName);
+    const articleModel = getModel(strapi, articleModelName);
+
+    const { id } = ctx.params;
+    const { body } = ctx.request;
+
+    let entity;
+    try {
+      entity = await service.findOne({ id });
+    } catch (error) {
+      return ctx.badRequest(formatError(error));
+    }
+
+    if (!entity) {
+      return ctx.badRequest(`Debate with "${id}" doesn't exist`);
+    }
+
+    let articleA;
+    try {
+      const oldArticleA = await articleService.findOne({ id: entity.articleA.id });
+      articleA = await articleService.update({ id: entity.articleA.id }, {
+        ...oldArticleA,
+        ...body.articleA,
+      });
+    } catch (error) {
+      return ctx.badRequest(formatError(error));
+    }
+
+    if (body.articleA) {
+      delete body.articleA;
+    }
+
+    let articleB;
+    try {
+      const oldArticleB = await articleService.findOne({ id: entity.articleB.id });
+      articleB = await articleService.update({ id: entity.articleB.id }, {
+        ...oldArticleB,
+        ...body.articleB,
+      });
+    } catch (error) {
+      return ctx.badRequest(formatError(error));
+    }
+
+    if (body.articleB) {
+      delete body.articleB;
+    }
+
+    ctx.request.body = body;
+
+    let debate;
+    try {
+      if (ctx.is('multipart')) {
+        const { data, files } = parseMultipartData(ctx);
+        debate = await service.update({ id }, data, {
+          files,
+        });
+      } else {
+        debate = await service.update({ id }, body);
+      }
+    } catch (error) {
+      return ctx.badRequest(formatError(error));
+    }
+
+    debate.articleA = sanitizeEntity(articleA, {
+      model: articleModel,
+    });
+
+    debate.articleB = sanitizeEntity(articleB, {
+      model: articleModel,
+    });
+
+    return sanitizeEntity(debate, {
+      model
+    });
   },
 };
