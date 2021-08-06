9be0bbf0a28327999afa7bcd0f7e053913918961
Daniel Pfisterer <info@daniel-pfisterer.de>
2021-08-06 13:14:05 +0200

    Temparary disable web socket

--- api/moderation/models/moderation.js
+++ api/moderation/models/moderation.js
@@ -1,32 +1,32 @@
 'use strict';
 
 const {
-  sanitizeEntity,
+  // sanitizeEntity,
 } = require('strapi-utils');
 
 module.exports = {
   lifecycles: {
-    afterCreate: async (result) => {
-      if (strapi.emitSocket && strapi.emitSocket.comment && strapi.emitSocket.comment.create) {
-        let comment = await strapi.services.comment.findOne({ id: result.comment.id });
-        comment = await strapi.services.comment.update({id: comment.id}, comment);
-        strapi.emitSocket.comment.create(sanitizeEntity(comment, {model: strapi.models.comment}));
-      }
-    },
-    afterUpdate: async (result) => {
-      if (strapi.emitSocket && strapi.emitSocket.comment.update) {
-        let comment = await strapi.services.comment.findOne({ id: result.comment.id });
-        // comment = await strapi.services.comment.update({id: comment.id}, comment);
-        comment.updated_at = new Date().toISOString();
-        strapi.emitSocket.comment.create(sanitizeEntity(comment, {model: strapi.models.comment}));
-      }
-    },
-    afterDelete: async(result) => {
-      if (strapi.emitSocket && strapi.emitSocket.comment.delete) {
-        let comment = await strapi.services.comment.findOne({ id: result.comment.id });
-        comment = await strapi.services.comment.update({id: comment.id}, comment);
-        strapi.emitSocket.comment.create(sanitizeEntity(comment, {model: strapi.models.comment}));
-      }
-    }
+    // afterCreate: async (result) => {
+    //   if (strapi.emitSocket && strapi.emitSocket.comment && strapi.emitSocket.comment.create) {
+    //     let comment = await strapi.services.comment.findOne({ id: result.comment.id });
+    //     comment = await strapi.services.comment.update({id: comment.id}, comment);
+    //     strapi.emitSocket.comment.create(sanitizeEntity(comment, {model: strapi.models.comment}));
+    //   }
+    // },
+    // afterUpdate: async (result) => {
+    //   if (strapi.emitSocket && strapi.emitSocket.comment.update) {
+    //     let comment = await strapi.services.comment.findOne({ id: result.comment.id });
+    //     // comment = await strapi.services.comment.update({id: comment.id}, comment);
+    //     comment.updated_at = new Date().toISOString();
+    //     strapi.emitSocket.comment.create(sanitizeEntity(comment, {model: strapi.models.comment}));
+    //   }
+    // },
+    // afterDelete: async(result) => {
+    //   if (strapi.emitSocket && strapi.emitSocket.comment.delete) {
+    //     let comment = await strapi.services.comment.findOne({ id: result.comment.id });
+    //     comment = await strapi.services.comment.update({id: comment.id}, comment);
+    //     strapi.emitSocket.comment.create(sanitizeEntity(comment, {model: strapi.models.comment}));
+    //   }
+    // }
   }
 };
